1579d9662b8c2c634b6c355d3acd32bc7b9ef719
ajmalg08 <ajmalg08@outlook.com>
2018-09-17 20:01:26 +0500

    ORM Configure is completed ,  Add Student Controller and View is creaded successfully

---
+++ ThetaProject/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThetaProject.Models;
+
+namespace ThetaProject.Controllers
+{
+
+    public class StudentController : Controller
+    {
+        private ProjectDBContext ORM = null;
+
+        public StudentController(ProjectDBContext ORM)
+        {
+            this.ORM = ORM;
+        }
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student S)
+        {
+            ORM.Student.Add(S);
+            ORM.SaveChanges();
+            ViewBag.Message = "New student has added to list";
+            return View();
+        }
+    }
+
+}
