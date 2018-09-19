95193ccc7317b4c0cc35229c2d595ed5b93853ee
ajmalg08 <ajmalg08@outlook.com>
2018-09-19 21:55:17 +0500

    Email send and searg by Name , deparatament and address is also done

--- ThetaProject/Controllers/StudentController.cs
+++ ThetaProject/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -44,12 +45,49 @@ namespace ThetaProject.Controllers
             S.Cv = "/WebData/CVs/" + CVName + CVExtension;
             ORM.Student.Add(S);
             ORM.SaveChanges();
+
+            MailMessage Obj = new MailMessage();
+            Obj.From = new MailAddress("meharsalman073@gamil.com");
+            Obj.To.Add(new MailAddress(S.Email));
+            Obj.Subject = "Welcome to theta Solution:";
+            Obj.Body = "Dear" + S.Name + "<br ><br >" +
+            "Thanks for registration with Theta Solution";
+            Obj.IsBodyHtml = true;
+            if (!string.IsNullOrEmpty(S.Cv))
+            {
+                Obj.Attachments.Add(new Attachment(ENV.WebRootPath + S.Cv));
+            }
+
+            SmtpClient SMTP = new SmtpClient();
+            SMTP.Host = "meharsalman073@gamil.com";
+            SMTP.Port = 587;
+            SMTP.EnableSsl = true;
+            SMTP.Credentials = new System.Net.NetworkCredential("meharsalman073@gmail.com", "salman123");
+
+            try
+            {
+                SMTP.Send(Obj);
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = "Mail has sent successfully";
+            }
+
+
             ViewBag.Message = "New student has added to list";
             return View();
         }
+        [HttpGet]
         public IActionResult AllStudents()
         {
             IList<Student> SS = ORM.Student.ToList<Student>();
+            return View(SS);
+        }
+        [HttpPost]
+        public IActionResult AllStudents(String SearchByName , String SearchByDept , String SearchByAddress)
+        {
+
+            IList<Student> SS = ORM.Student.Where(m => m.Name.Contains(SearchByName) || m.Dept.Contains(SearchByDept) || m.Address.Contains(SearchByAddress)).ToList<Student>();
             return View(SS);
         }
         public IActionResult DetailStudent(int Id)
@@ -95,6 +133,8 @@ namespace ThetaProject.Controllers
 
             return File(Id , M.Lookup(Id), Name);
         }
+       
+        
         
     }
 
